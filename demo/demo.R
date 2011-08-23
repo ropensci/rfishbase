c1dc9a89bb1e25aaa00d30efe204dca81abb1624
Carl Boettiger <cboettig@gmail.com>
2011-08-23 11:23:45 -0700

    demo.R  autocommit

--- demo/demo.R
+++ demo/demo.R
@@ -1,14 +1,34 @@
-require(fishbase)
+# demo.R
 
+require(fishbase)
 require(socialR)
 script <- "demo.R" # Must specify the script name! 
 gitaddr <- gitcommit(script)     # Must commmit at start and store id.
 
 
+getData <- function(fish.ids){
+  suppressWarnings(
+    lapply(fish.ids, function(i) try(fishbase(i)))
+  )
+}
 
-weights(2:500)->a
+fish.data <- getData(2:500)
+get.ages <- function(fish.data){
+              sapply(fish.data, function(out){
+                 if(!(is(out,"try-error")))
+                   x <- out$size_values[["age"]]
+                 else
+                   x <- NA
+                 x
+               })
+  
+}
 
-png("weights.png"); 
-hist(log(a), breaks=40, main="Weight Distribution", xlab="Log mass (g)"); 
+yr <- get.ages(fish.data)
+png("age.png"); 
+hist(yr, breaks=40, main="Age Distribution", xlab="age (years)"); 
 dev.off()
-upload("weights.png", script=script, gitaddr=gitaddr)
+upload("age.png", script=script, gitaddr=gitaddr)
+
+
+
