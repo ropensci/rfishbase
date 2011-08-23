8939e068a0c649c2326f262115b6175c3c0c9185
Carl Boettiger <cboettig@gmail.com>
2011-08-23 09:41:53 -0700

    working fishbase example

--- R/fishbase.R
+++ R/fishbase.R
@@ -7,6 +7,12 @@ fishbase <- function(fish.id, curl=getCurlHandle()){
   # Query the XML page of fishbase given a fishbase id 
   # and return the XML R object
   #
+  # common name, family, max length, max published weight, max
+  # reported age, length at first maturity, range of length at first
+  # maturity, environment, climate/range, distribution, short description,
+  # and biology.
+  #
+  #
   # Examples: 
   #   NileTilapia <- fishbase("2")
   #   metadata(tree$S.id)
@@ -14,11 +20,38 @@ fishbase <- function(fish.id, curl=getCurlHandle()){
   url <- paste("http://fishbase.sinica.edu.tw/",
                "maintenance/FB/showXML.php?identifier=FB-",
               fish.id, "&ProviderDbase=03", sep="")
+
+  # these are probably guessed by xmlNamespaceDefinitions in the xpath functions
+  namespaces <- c(xsd="http://www.w3.org/2001/XMLSchema", 
+                  dc="http://purl.org/dc/elements/1.1/", 
+                  dcterms="http://purl.org/dc/terms/", 
+                  geo="http://www.w3.org/2003/01/geo/wgs84_pos#", 
+                  dwc="http://rs.tdwg.org/dwc/dwcore/", 
+                  xsi="http://www.w3.org/2001/XMLSchema-instance", 
+                  schemaLocation="http://www.eol.org/transfer/content/0.1")
+
+
   tt <- getURLContent(url, followlocation=TRUE, curl=curl)
   doc <- xmlParse(tt)
-  dc = getNodeSet(doc, "//")
-#  dc = getNodeSet(doc, "//dc:dc", namespaces=c(dc="http://www.openarchives.org/OAI/2.0/oai_dc/"))
-  lapply(dc, xmlToList)
- }
+  Genus <- xmlValue(getNodeSet(doc, "//dwc:Genus", 
+                  namespaces=namespaces)[[1]])
+  Family <- sapply(getNodeSet(doc, "//dwc:Family", 
+                   namespaces=namespaces), xmlToList) 
+  ScientificName <- sapply(getNodeSet(doc, "//dwc:ScientificName", 
+                           namespaces=namespaces), xmlToList)
+ 
+  # Size information: go to the parent node of the dc:identifier node
+  # which has FB-Size-2 property.  The child of that node which is a dc:description
+  # is where we'll find the size information
+  size_node <- getNodeSet(doc, "//dc:identifier[string(.) = 'FB-Size-2']/..", namespaces)
+  size <- xmlValue( size_node[[1]][["description"]] )
+  # Likewise for distribution information
+  dist_node <- getNodeSet(doc, "//dc:identifier[string(.) = 'FB-Distribution-2']/..", namespaces)
+  distribution <- xmlValue( dist_node[[1]][["description"]] )
+
+  # Format the output 
+ list(Genus=Genus, Family=Family, ScientificName=ScientificName,
+      distribution=distribution, size=size)
+}
 
 
