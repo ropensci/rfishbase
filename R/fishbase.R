aab96761f8b5f218b2758d461bef9b304d05a336
Carl Boettiger <cboettig@gmail.com>
2011-08-26 16:57:41 -0700

    habitat counting

--- R/fishbase.R
+++ R/fishbase.R
@@ -30,57 +30,84 @@ fishbase <- function(fish.id, curl=getCurlHandle()){
 
   tt <- getURLContent(url, followlocation=TRUE, curl=curl)
   doc <- xmlParse(tt)
-  Genus <- xmlValue(getNodeSet(doc, "//dwc:Genus", 
-                  namespaces=namespaces)[[1]])
-  Family <- xmlValue(getNodeSet(doc, "//dwc:Family", 
-                   namespaces=namespaces)[[1]]) 
-  ScientificName <- sapply(getNodeSet(doc, "//dwc:ScientificName", 
-                           namespaces=namespaces), xmlToList)
- 
-  # Size information: go to the parent node of the dc:identifier node
-  # which has FB-Size-2 property.  The child of that node which is a dc:description
-  # is where we'll find the size information
-  size_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
-                          'FB-Size-", fish.id, "']/..", sep=""), namespaces)
-  size <- xmlValue( size_node[[1]][["description"]] )
+  Genus <- try(xmlValue(getNodeSet(doc, "//dwc:Genus", 
+                  namespaces=namespaces)[[1]]))
+
+  ## lets see if the page exists.
+  if(is(Genus, "try-error")){
+    output <- "try-error"
+  } else {
+    Family <- xmlValue(getNodeSet(doc, "//dwc:Family", 
+                     namespaces=namespaces)[[1]]) 
+    ScientificName <- sapply(getNodeSet(doc, "//dwc:ScientificName", 
+                             namespaces=namespaces), xmlToList)
+   
+    # Size information: go to the parent node of the dc:identifier node
+    # which has FB-Size-2 property.  The child of that node which is a dc:description
+    # is where we'll find the size information
+    size_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-Size-", fish.id, "']/..", sep=""), namespaces)
+    size <- xmlValue( size_node[[1]][["description"]] )
 
 
-  # Have to do some grep processing to extract the size information 
-  # as numbers instead of this text description.
-  length <- gsub("^((\\d|,)*\\.?\\d*) cm.+", "\\1", size)
-  length <- as.numeric(gsub(",", "", length)) # remove commas
+    # Have to do some grep processing to extract the size information 
+    # as numbers instead of this text description.
+    length <- gsub("^((\\d|,)*\\.?\\d*) cm.+", "\\1", size)
+    length <- as.numeric(gsub(",", "", length)) # remove commas
 
-  units <- gsub(".+published weight: \\d.* (g|kg).+", "\\1", size)
-  weight <- gsub(".+published weight: ((\\d|,)*\\.?\\d*) (g|kg).+", "\\1", size)
-  weight <- as.numeric(gsub(",", "", weight)) # remove commas
-  if(units=="kg") 
-    weight <- weight*1000
-  age <-  as.numeric(gsub(".+reported age: (\\d.*) years.*", "\\1", size))
-  size_values <- c(length=length, weight=weight, age=age)  
+    units <- gsub(".+published weight: \\d.* (g|kg).+", "\\1", size)
+    weight <- gsub(".+published weight: ((\\d|,)*\\.?\\d*) (g|kg).+", "\\1", size)
+    weight <- as.numeric(gsub(",", "", weight)) # remove commas
+    if(units=="kg") 
+      weight <- weight*1000
+    age <-  as.numeric(gsub(".+reported age: (\\d.*) years.*", "\\1", size))
+    size_values <- c(length=length, weight=weight, age=age)  
 
-  # Likewise for distribution information 
-  dist_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
-                          'FB-Distribution-", fish.id, "']/..", sep=""),
-                          namespaces)
-  distribution <- xmlValue( dist_node[[1]][["description"]] )
-  # The dist_node may have other useful information, including maps 
-
-  # Likewise for distribution information 
-  habitat_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
-                          'FB-Habitat-", fish.id, "']/..", sep=""), namespaces)
-  habitat <- xmlValue( habitat_node[[1]][["description"]] )
-  # The dist_node may have other useful information, including maps 
+    # Likewise for distribution information 
+    dist_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-Distribution-", fish.id, "']/..", sep=""),
+                            namespaces)
+    distribution <- xmlValue( dist_node[[1]][["description"]] )
+    # The dist_node may have other useful information, including maps 
 
 
+    # May want to mine this for keywords in a subsequent function
+    habitat_node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-Habitat-", fish.id, "']/..", sep=""), namespaces)
+    habitat <- xmlValue( habitat_node[[1]][["description"]] )
+
+    
+    node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-TrophicStrategy-", fish.id, "']/..", sep=""), namespaces)
+    trophic <- xmlValue( node[[1]][["description"]] )
+
+    node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-LifeCycle-", fish.id, "']/..", sep=""), namespaces)
+    lifecycle <- xmlValue( node[[1]][["description"]] )
+
+    node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-Morphology-", fish.id, "']/..", sep=""), namespaces)
+    morphology <- xmlValue( node[[1]][["description"]] )
+
+    node <- getNodeSet(doc, paste("//dc:identifier[string(.) =
+                            'FB-DiagnosticDescr-", fish.id, "']/..", sep=""), namespaces)
+    diagnostic <- xmlValue( node[[1]][["description"]] )
+  
   # Format the output 
- list(Genus=Genus, Family=Family, ScientificName=ScientificName,
-      distribution=distribution, size=size, size_values=size_values)
+  output <- list(Genus=Genus, Family=Family, ScientificName=ScientificName,
+      distribution=distribution, size=size, size_values=size_values,
+      habitat=habitat, trophic=trophic, lifecycle=lifecycle, 
+      morphology=morphology, diagnostic=diagnostic, id=fish.id)
+  }
+  output
 }
 
 
-## a general function to loop over all fish ids to get data
-## Drops the missing entries
 getData <- function(fish.ids){
+# Description: a general function to loop over all fish ids to get data
+#   also drops the missing entries
+# Example:
+#   all.fishbase <- getData(1:30000)
   data <- 
   suppressWarnings(
     lapply(fish.ids, function(i) try(fishbase(i)))
--- demo/demo.R
+++ demo/demo.R
@@ -13,6 +13,9 @@ get.ages <- function(fish.data){
 
 # Process all the XML first, then extract the ages 
 fish.data <- getData(2:100)
+
+
+
 yr <- get.ages(fish.data)
 
 # Plot data
@@ -21,7 +24,36 @@ hist(yr, breaks=40, main="Age Distribution", xlab="age (years)");
 dev.off()
 
 
-## Log in notebook for Reproducible Research 
+
+
+
+
+
+# Now let's just grab the entire fishbase database, 
+fish.data <- getData(1:30000)
+
+
+habitatSearch <- function(keyword, fish.data){
+# A function to search for the occurances of a keyword in habitat description
+# Args:
+#   keyword: pattern to be used by grep
+#   fish.data: list of outputs from fishbase(), or from getData()
+# Example:
+#   data <- getData(1:10)
+#   habitatSearch("feshwater", data)
+  x <- sapply(fish.data, function(x) grep(keyword, x$habitat) )
+  x <- as.integer(x)
+}
+
+
+x <- habitatSearch("reef", fish.data)
+reef <- sum(x, na.rm=T)
+nonreef <- sum(is.na(x))
+percent_reef <- reef/(reef+nonreef) 
+
+
+
+## Log in lab notebook for Reproducible Research 
 require(socialR)
 script <- "demo.R" # Must specify the script name! 
 gitaddr <- gitcommit(script)     # Must commmit at start and store id.
