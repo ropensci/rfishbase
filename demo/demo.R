d8258935287469f1400323f4e4297aa985b8041f
Carl Boettiger <cboettig@gmail.com>
2011-09-01 12:16:06 -0700

    updated demo

--- demo/demo.R
+++ demo/demo.R
@@ -12,9 +12,6 @@ get.ages <- function(fish.data){
 
 # Process all the XML first, then extract the ages 
 fish.data <- getData(2:40)
-
-
-
 yr <- get.ages(fish.data)
 
 # Plot data
@@ -22,24 +19,8 @@ png("age.png");
 hist(yr, breaks=40, main="Age Distribution", xlab="age (years)"); 
 dev.off()
 
-
-
-
-
-
-#require(socialR)
-#tweet("Downloading fishbase ids 20,000 : 30,000")
-# Now let's just grab the entire fishbase database, 
-fish.dataC <- getData(20001:30000)
-#tweet(paste("Done downloading ", length(fish.dataC), "entries"))
-fish.dataB <- getData(10001:20000)
-#tweet(paste("Done downloading ", length(fish.dataB), "entries"))
-fish.dataA <- getData(1:10000)
-#tweet(paste("Done downloading ", length(fish.dataA), "entries"))
-fish.data <- c(fish.dataA, fish.dataB, fish.dataC)
-
 habitatSearch <- function(keyword, fish.data){
-# A function to search for the occurances of a keyword in habitat description
+# A function to search for the occurances of any keyword in habitat description
 # Args:
 #   keyword: pattern to be used by grep
 #   fish.data: list of outputs from fishbase(), or from getData()
@@ -50,19 +31,11 @@ habitatSearch <- function(keyword, fish.data){
   x <- as.integer(x)
 }
 
+is.fresh <- habitatSearch("freshwater", fish.data)
+freshwater <- sum(is.fresh, na.rm=T)
+is.marine <- habitatSearch("marine", fish.data)
+freshwater <- sum(is.marine, na.rm=T)
 
-x <- habitatSearch("reef", fish.data)
-reef <- sum(x, na.rm=T)
-nonreef <- sum(is.na(x))
-percent_reef <- reef/(reef+nonreef) 
-
-save(list=ls(), file="fishbase.Rdat")
-
-## Log in lab notebook for Reproducible Research 
-require(socialR)
-script <- "demo.R" # Must specify the script name! 
-gitaddr <- gitcommit(script)     # Must commmit at start and store id.
-#upload("age.png", script=script, gitaddr=gitaddr)
 
 
 
---
+++ demo/downloadFishbase.R
@@ -0,0 +1,24 @@
+# demo.R
+require(rfishbase)
+require(socialR)
+tweet("Downloading fishbase ids 20,000 : 30,000")
+
+# Now let's just grab the entire fishbase database, 
+fish.dataC <- getData(20001:30000)
+tweet(paste("C: Done downloading ", length(fish.dataC), "entries"))
+save(list="fish.dataC", file="fishC.Rdat")
+rm(list=ls())
+
+# Now let's just grab the entire fishbase database, 
+fish.dataB <- getData(10001:20000)
+tweet(paste("B: Done downloading ", length(fish.dataB), "entries"))
+save(list="fish.dataB", file="fishB.Rdat")
+rm(list=ls())
+
+
+# Now let's just grab the entire fishbase database, 
+fish.dataB <- getData(1:10000)
+tweet(paste("A: Done downloading ", length(fish.dataA), "entries"))
+save(list="fish.dataA", file="fishA.Rdat")
+
+
