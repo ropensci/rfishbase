cf12e1986eca43c9c7fca8fd81b90349bcf96542
Carl Boettiger <cboettig@gmail.com>
2011-09-02 12:53:49 -0700

    updated download example to include fusing files back together

--- demo/downloadFishbase.R
+++ demo/downloadFishbase.R
@@ -1,13 +1,24 @@
-# demo.R
+# downloadFishbase.R
 rm(list=ls())
 require(rfishbase)
-# may be helpful to run chunks at different times
+
+## may be helpful to run chunks at different times if this errors
 for(i in 1:14){
 	M <- ceiling(70000/14)
 	ids <- (1+(i-1)*M):(i*M)
 	fish.data <- getData(ids, silent=FALSE)
 	save(list="fish.data", file=paste("fishdata_", i, ".Rdat", sep=""))
-	rm(list="fish.data")
+	rm(list="fish.data") # clear some memory
 }
 
 
+###  Fuse and timestamp the collected chunks 
+load("fishdata_1.Rdat")
+all.fishbase <- fish.data 
+for(i in 2:14){
+	load(paste("fishdata_", i, ".Rdat", sep = ""))
+	 all.fishbase <- c(fish.data, all.fishbase) 
+}
+
+print(length(all.fishbase))
+save(list="all.fishbase", file=paste(Sys.Date(), "-fishbase", ".rda")
