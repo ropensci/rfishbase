39a8a6645187855cbaaa82c90b9496900031e8ce
Carl Boettiger <cboettig@gmail.com>
2011-10-10 10:52:33 -0700

    Updated database, one-shot download now working smoothly

--- demo/downloadFB.R
+++ demo/downloadFB.R
@@ -2,6 +2,6 @@
 rm(list=ls())
 require(rfishbase)
 fish.data <- getData(1:70000, silent=FALSE)
-save(list="fish.data", file=paste(SysDate(), "fishdata.Rdat", sep=""))
+save(list="fish.data", file=paste(Sys.Date(), "fishdata.Rdat", sep=""))
 
 
