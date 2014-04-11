0ec7b6f45e931994099bb952da66f28a5ecfe865
Carl Boettiger <cboettig@gmail.com>
2014-04-11 13:45:48 -0700

    fix typo in test

--- tests/testthat/test_trophiclevel.R
+++ tests/testthat/test_trophiclevel.R
@@ -7,7 +7,7 @@ context("trophic level from HTML scraping")
 
 test_that("we can execute getTrophicLevel", {
   response <- getTrophicLevel(fish.data[1:2])
-  expect_equals(response[[1]], 2) 
+  expect_equal(response[[1]], 2) 
 })
 
 ## Should test that all the optional arguments to getTrophicLevel work as expected too...
