03dbd53e7b5f39c4c08e9ebb046a07a3fc16258a
Carl Boettiger <cboettig@gmail.com>
2015-02-05 21:06:36 -0800

    working on fao area table

--- R/fao_area.R
+++ R/fao_area.R
@@ -4,12 +4,20 @@
 codes <- species_table(species_list, fields="SpecCode")$SpecCode
 do.call("rbind", lapply(codes, per_fao))
 
-per_fao <- function(code, server = SERVER, verbose = TRUE, limit = 100){
+faoareas <- function(code, server = SERVER, verbose = TRUE, limit = 100){
   resp <- GET(paste0(server, "/faoareas"), query = list(SpecCode = code, limit = limit))
   
   data <- check_and_parse(resp, verbose = verbose)
   
+  lapply(data$AreaCode, faoarrefs)
   
-  resp <- GET(paste0(server, "/faoarrefs"), query = list(SpecCode = code, limit = limit))
+  ## Look up area codes
   
+  
+}
+
+faoarrefs <- function(area_code, server = SERVER, verbose = TRUE, limit = 100){
+  ## add in a fields list to filter returned values
+  resp <- GET(paste0(server, "/faoarrefs/", area_code), query = list(limit = limit))
+  data <- check_and_parse(resp)
 }
