b922f3f28ab06660199e9702b9abd9f7ebf271e8
Carl Boettiger <cboettig@gmail.com>
2015-02-08 22:33:07 -0800

    commonnames much better. joins against species names, still needs to replace C_code with countrycode. sci_to_common fully implemented, deals with multiple matches by returning all scientific names

--- R/commonnames.R
+++ R/commonnames.R
@@ -1,8 +1,14 @@
-## Function name suggests we are looking up common name, not giving it
-# common_to_sci(c("Bicolor cleaner wrasse", "humphead parrotfish"))
-common_to_sci <- function(x, Language = NULL, verbose = TRUE, limit = 10, server = SERVER){
+#' 
+#' @examples
+#' \donttest{
+#' common_to_sci(c("Bicolor cleaner wrasse", "humphead parrotfish"))
+#' common_to_sci("trout")
+#' common_to_sci(c("trout", "Coho Salmon"))
+#' }
+#' @seealso \code{\link{commonnames}}, \code{\link{species_list}}, \code{\link{synonyms}}
+common_to_sci <- function(x, Language = NULL, verbose = TRUE, limit = 1000, server = SERVER){
   
-  sapply(x, function(x){
+  out <- sapply(x, function(x){
     # Look up SpecCode for the common names
     resp <- GET(paste0(server, "/comnames"), 
                 query = list(ComName = x,
@@ -10,35 +16,51 @@ common_to_sci <- function(x, Language = NULL, verbose = TRUE, limit = 10, server
                              limit = limit,  # SpecCode same for all matches
                              fields = 'SpecCode'))
     data <- check_and_parse(resp, verbose = verbose)
+    matches <- unique(data[[1]])
     
-    # Check if we have gotten different species codes for the same
-    # FIXME this isn't ideal behavior. Do something more intelligent
-    if(!all(data[[1]] == data[[1]][[1]]))
-      warning("Common name matches multiple scientific names, using first match")
-    
-    ## Grab the first code
-    code <- data[[1]][[1]]
-    # Turn SpecCode into scientific name. Use local taxa table for speed
-    species_list(SpecCode = code)
+    sci_names <- species_info(matches, fields = c("Genus", "Species"))
+    unite_(sci_names, "sci_name", c("Genus", "Species"), sep = " ")$sci_name
   })
   
+  # If multiple matches are found, we want to collapse
+  unname(unlist(out))
 }
 
 
-
-
-## Note that there are many common names for a given sci name, so sci_to_common doesn't make sense
-## FIXME consider if there are additional fields we want to add here.
-find_commonnames <- function(species_list, verbose = TRUE, limit = 100, server = SERVER){
+#' Return a table of common names
+#' @inheritParams species_info
+#' @details Note that there are many common names for a given sci name, so sci_to_common doesn't make sense
+#' 
+#' @examples
+#' \donttest{
+#' commonnames(c("Labroides bicolor",  "Bolbometopon muricatum"))
+#' 
+#' # subset by English language names
+#' fish <- commonnames("Bolbometopon muricatum")
+#' library(dplyr)
+#' fish %>% filter(Language=="English") 
+#' }
+#
+## FIXME C_Code should be mapped to a country name using the table 'country' 
+commonnames <- function(species_list, 
+                        verbose = TRUE, 
+                        limit = 100, 
+                        server = SERVER, 
+                        fields = c('ComName', 'Language','C_Code', 'SpecCode')){
   
+  codes <- speccodes(species_list)
   
-  codes <- speccodes_for_species(species_list)
   bind_rows(lapply(codes, function(code){
     resp <- GET(paste0(server, "/comnames"), 
                 query = list(SpecCode = code, 
                              limit = limit, 
-                             fields = 'SpecCode,ComName,Language'))
-    check_and_parse(resp, verbose = verbose)
+                             fields = paste(fields, collapse=",")))
+    df <- check_and_parse(resp, verbose = verbose)
+    
+    # Replace / Join SpecCode with Genus and Species columns
+    id_df <- select_(taxa(query = list(SpecCode = code)), "Genus", "Species", "SpecCode")
+    df <- left_join(df, id_df)
+    
+    # Replace C_Code with Country
   }))
 }
-# N <- find_commonnames(c("Labroides bicolor"))
