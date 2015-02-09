0eade50b6fa9fceb7108774772e57f757776b596
Carl Boettiger <cboettig@gmail.com>
2015-02-08 22:33:32 -0800

    heartbeat and ping exported, examples show testing timing etc

--- R/heartbeat.R
+++ R/heartbeat.R
@@ -1,10 +1,40 @@
-# Check that server is responding
-
+#' heartbeat
+#' 
+#' Check that the FishBase API server is responding
+#' @inheritParams species_info
+#' @return An httr 'response' object.
+#' @examples
+#' \dontrun{
+#' 
+#' ## Show server response times
+#' resp <- heartbeat()
+#' resp$times
+#' 
+#' ## Show API endpoints:
+#' content(resp)
+#' 
+#' }
+#' @import httr
+#' @export
 heartbeat <- function(server = SERVER)
   GET(paste0(server, "/heartbeat"))
 
+#' ping
+#' 
+#' Check that the FishBase MySQL backend to the API is alos responding
+#' @inheritParams species_info
+#' @return An httr 'response' object.
+#' @examples
+#' \dontrun{
+#' 
+#' ## Show server response times
+#' resp <- ping()
+#' resp$times
+#' 
+#' }
+#' @import httr
+#' @export
 ping <- function(server = SERVER)
   GET(paste0(server, "/mysqlping"))
 
-# Consider echoing resp$times[["total"]] for time in seconds
-# Full resp info has useful debugging record. 
+
