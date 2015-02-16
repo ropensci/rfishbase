96f82bd13368abc357b67f44d59331d7888bb1c1
Carl Boettiger <cboettig@gmail.com>
2015-02-16 15:07:18 -0800

    more tests for error checks

---
+++ tests/testthat/test_check_and_parse.R
@@ -0,0 +1,38 @@
+context("error checks")
+
+
+
+test_that("Waning message and no proceed if parsing fails", {
+  parsed <- "parsed is a character error message"
+  expect_warning(proceed <- error_checks())
+  expect_false(proceed)  
+})
+
+test_that("Waning message and no proceed if parsing is empty", {
+  parsed <- list()
+  expect_warning(proceed <- error_checks(parsed))
+  expect_false(proceed)  
+})
+
+
+test_that("Waning message and no proceed if parsing discovers that API returned an error", {
+  parsed <- list(error = "An error message")
+  expect_warning(proceed <- error_checks(parsed))
+  expect_false(proceed)  
+})
+
+
+test_that("Waning message and no proceed if parsing discovers that returned empty", {
+  parsed <- list(count = 0)
+  expect_warning(proceed <- error_checks(parsed))
+  expect_false(proceed)  
+})
+
+
+test_that("Waning message but proceed if only partial list returned because limit was reached", {
+  parsed <- list(count = 10, returned = 5)
+  expect_warning(proceed <- error_checks(parsed))
+  expect_true(proceed)  
+})
+
+
