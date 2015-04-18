e9266f171e6bf63d6b91cdcecb7037b3db59a777
Carl Boettiger <cboettig@gmail.com>
2015-04-18 10:45:05 -0400

    set user-agent differently for automated tests, closes #40

--- R/00-endpoint.R
+++ R/00-endpoint.R
@@ -2,14 +2,14 @@
 ## Allows us to define functions for each endpoint using closures
 endpoint <- function(endpt, tidy_table = function(x) x){
   
-  function(species_list, fields = NULL, limit = 200, server = SERVER){
+  function(species_list, fields = NULL, limit = 200, server = SERVER, ...){
     codes <- speccodes(species_list)
     bind_rows(lapply(codes, function(code){ 
       args <- list(SpecCode = code,
                    limit = limit, 
                    fields = paste(fields, collapse=","))
       
-      resp <- GET(paste0(server, "/", endpt), query = args, user_agent(make_ua()))
+      resp <- GET(paste0(server, "/", endpt), query = args, ..., user_agent(make_ua()))
       data <- check_and_parse(resp)
       
       tidy_table(data)
@@ -20,9 +20,13 @@ endpoint <- function(endpt, tidy_table = function(x) x){
 
 ## Function to create the user-agent string
 make_ua <- function() {
+  if(getOption("is_test", FALSE)){
+    "automated_test"
+  } else {
   versions <- c(rfishbase = as.character(packageVersion("rfishbase")),
                 curl = RCurl::curlVersion()$version,
                 Rcurl = as.character(packageVersion("RCurl")),
                 httr = as.character(packageVersion("httr")))
   paste0(names(versions), "/", versions, collapse = " ")
+  }
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(rfishbase)
-
+options(is_test = TRUE) # set user-agent to distinguish automated tests
 test_check("rfishbase")
