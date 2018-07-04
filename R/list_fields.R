c6222559781d25fee0bda6dd4e78f69964a9b089
Carl Boettiger <cboettig@gmail.com>
2018-07-04 16:07:12 -0700

    document use of grep with list_fields, closes #105

--- R/list_fields.R
+++ R/list_fields.R
@@ -5,12 +5,17 @@
 #' @param  server base URL to the FishBase API (by default). For SeaLifeBase, use https://fishbase.ropensci.org/sealifebase 
 #' @param implemented_only by default, only return those tables that have been implemented.  
 #' @return a data frame listing the table names (matching function names in rfishbase) and the matching column names those tables have implemented.
-#' @details method will use partial matching. Hence "Temp" will match column names such as "TempMin" and "TempMax", but "MinTemp" will not.  Likewise,
-#' neither "Minimum" or "Temperature" will match "TempMin", so begin with the shortest query possible and refine based on search results when necessary.
-#' Note also that there is no guarentee that the same column has the same value or same meaning in different tables. 
+#' @details 
+#' Calling `list_fields()` with no arguments will return the full table of all known fields.
+#' Then users can employ standard filter techniques like grep for partial name matching; 
+#' see examples.
 #' @examples 
 #' \donttest{
 #' list_fields("Temp")
+#' 
+#' ## Regex matching on full table
+#' library(dplyr)
+#' list_fields() %>% filter(grepl("length", columns, ignore.case = TRUE))
 #' }
 #' @export
 list_fields <- function(fields = NULL,  server = getOption("FISHBASE_API", FISHBASE_API), 
