5f8a331c86fbcb8ada9b77be0ddb7d1e874e00f6
Carl Boettiger <cboettig@gmail.com>
2018-07-20 21:03:05 +0000

    here we go, time to merge :rocket:

--- R/load_taxa.R
+++ R/load_taxa.R
@@ -8,7 +8,7 @@
 #' @export
 load_taxa <- memoise::memoise(function(server = NULL, ...){
 
-  ## Handle versioning
+  ## SeaLifeBase requires a different taxa table function:
   if(is.null(server)) server <- getOption("FISHBASE_API", FISHBASE_API)
   if(grepl("sealifebase", server)){
     slb_taxa_table(server)
@@ -17,24 +17,42 @@ load_taxa <- memoise::memoise(function(server = NULL, ...){
   }
 })
   
+
+
+
+globalVariables(c("SpecCode", "Species", "Genus", "Subfamily", "Family", 
+                  "Order", "Class", "SuperClass", "Phylum", "Kingdom"))
+
+
 fb_taxa_table <- function(server){
   taxon_species <- fb_tbl("species", server)
-  keep <- names(taxon_species) %in% c("SpecCode", "Species", "Genus", "Subfamily",
-                                      "GenCode", "SubGenCode", "FamCode")
+  keep <- names(taxon_species) %in% 
+    c("SpecCode", "Species", "Genus", "Subfamily",
+      "GenCode", "SubGenCode", "FamCode")
   taxon_species <- taxon_species[keep]
   
   taxon_genus <- fb_tbl("genera", server) 
-  keep <- names(taxon_genus) %in% c("GenCode", "GenName", "GenComName", "FamCode",
-                                    "Subfamily", "SubgenusOf")
+  keep <- names(taxon_genus) %in% 
+    c("GenCode", "GenName", "GenComName", "FamCode",
+      "Subfamily", "SubgenusOf")
   taxon_genus <- taxon_genus[keep]
   i <- names(taxon_genus) == "GenComName"
   names(taxon_genus)[i] <- "GenusCommonName" 
   
-  taxon_family <- fb_tbl("families", server) %>% 
-    select(FamCode, Family, FamilyCommonName = CommonName, Order, Ordnum, Class, ClassNum) 
+  taxon_family <- fb_tbl("families", server)
+  keep <- names(taxon_family) %in% 
+    c("FamCode", "Family","CommonName", "Order",
+      "Ordnum", "Class", "ClassNum")
+  taxon_family <- taxon_family[keep]
+  i <- names(taxon_family) == "CommonName"
+  names(taxon_family)[i] <- "FamilyCommonName"
   
-  taxon_order <- fb_tbl("orders", server) %>% select(Ordnum, Order, OrderCommonName = CommonName, ClassNum, Class) 
-  
+  taxon_order <- fb_tbl("orders", server)
+  keep <- names(taxon_order) %in% 
+    c("Ordnum", "Order", "CommonName", "ClassNum", "Class") 
+  taxon_order <- taxon_order[keep]
+  i <- names(taxon_order) == "CommonName"
+  names(taxon_order)[i] <- "OrderCommonName"
   
   taxon_class <- fb_tbl("classes", server)
   keep <- names(taxon_class) %in% c("ClassNum", "Class", "CommonName",
@@ -64,9 +82,6 @@ fb_taxa_table <- function(server){
 }
 
 
-globalVariables(c("SpecCode", "Species", "Genus", "Subfamily", "Family", 
-        "Order", "Class", "SuperClass"))
-        
 
 
 slb_taxa_table <- function(server){
@@ -78,52 +93,68 @@ slb_taxa_table <- function(server){
   taxon_family <- fb_tbl("families", server)
   taxon_order <- fb_tbl("orders", server)
   taxon_class <- fb_tbl("classes", server)
-
-
-keep <- names(taxon_species) %in% c("SpecCode", "Species", "Genus",
-                                    "GenCode", "SubGenCode", "FamCode")
-taxon_species <- taxon_species[keep]
-keep <- names(taxon_genus) %in% c("GenCode", "GEN_NAME", "GenComName", "FamCode",
-                                  "Subfamily", "SubgenusOf")
-taxon_genus <- taxon_genus[keep]
-i <- names(taxon_genus) == "GenComName"
-names(taxon_genus)[i] <- "GenusCommonName" 
-i <- names(taxon_genus) == "GEN_NAME"
-names(taxon_genus)[i] <- "Genus" 
-
-left_join(taxon_species, taxon_genus)
-
-taxon_family <- fb_tbl("families", server) %>% 
-  select(FamCode, Family, FamilyCommonName = CommonName, Order, Ordnum, Class, ClassNum) 
-
-taxon_order <- fb_tbl("orders", server) %>% select(Ordnum, Order, OrderCommonName = CommonName, ClassNum, Class) 
-
-
-taxon_class <- fb_tbl("classes", server)
-keep <- names(taxon_class) %in% c("ClassNum", "Class", "CommonName",
-                                  "SuperClass", "Subclass")
-i <- names(taxon_class) == "CommonName"
-names(taxon_class)[i] <- "ClassCommonName"
-taxon_class <- taxon_class[keep]
-
-
-suppressMessages(
-  taxon_hierarchy <- 
-    taxon_species %>%
-    left_join(taxon_genus) %>%
-    left_join(taxon_family )%>%
-    left_join(taxon_order) %>%
-    left_join(taxon_class)
-)
-
-taxa_table <- 
-  taxon_hierarchy %>% 
-  dplyr::select(SpecCode, Species, Genus, Subfamily, Family, 
-                Order, Class) %>% 
-  dplyr::arrange(SpecCode) %>% 
-  dplyr::mutate(Species = paste(Genus, Species))
-
-taxa_table
+  taxon_phylum <- fb_tbl("phylums", server)
+  
+  
+  keep <- names(taxon_species) %in% 
+    c("SpecCode", "Species", "Genus",
+      "GenCode", "SubGenCode", "FamCode")
+  taxon_species <- taxon_species[keep]
+  
+  
+  keep <- names(taxon_genus) %in% 
+    c("GenCode", "GEN_NAME", "GenComName", "FamCode",
+      "Subfamily", "SubgenusOf")
+  taxon_genus <- taxon_genus[keep]
+  i <- names(taxon_genus) == "GenComName"
+  names(taxon_genus)[i] <- "GenusCommonName" 
+  i <- names(taxon_genus) == "GEN_NAME"
+  names(taxon_genus)[i] <- "Genus" 
+  
+  keep <- names(taxon_family) %in% 
+    c("FamCode", "Family","CommonName", "Order",
+      "Ordnum", "Class", "ClassNum")
+  taxon_family <- taxon_family[keep]
+  i <- names(taxon_family) == "CommonName"
+  names(taxon_family)[i] <- "FamilyCommonName"
+  
+  keep <- names(taxon_order) %in% 
+    c("Ordnum", "Order", "CommonName", "ClassNum", "Class") 
+  taxon_order <- taxon_order[keep]
+  i <- names(taxon_order) == "CommonName"
+  names(taxon_order)[i] <- "OrderCommonName"
+  
+  keep <- names(taxon_class) %in% 
+    c("ClassNum", "Class", "CommonName",
+      "SuperClass", "Subclass")
+  i <- names(taxon_class) == "CommonName"
+  names(taxon_class)[i] <- "ClassCommonName"
+  taxon_class <- taxon_class[keep]
+  
+  keep <- names(taxon_phylum) %in% 
+    c("PhylumId", "Phylum", "Kingdom")
+  i <- names(taxon_phylum) == "CommonName"
+  names(taxon_phylum)[i] <- "PhylumCommonName"
+  taxon_phylum <- taxon_phylum[keep]
+  
+  suppressMessages(
+    taxon_hierarchy <- 
+      taxon_species %>%
+      left_join(taxon_genus) %>%
+      left_join(taxon_family )%>%
+      left_join(taxon_order) %>%
+      left_join(taxon_class) %>%
+      left_join(taxon_phylum)
+  )
+  
+  taxa_table <- 
+    taxon_hierarchy %>% 
+    dplyr::select(SpecCode, Species, Genus, Subfamily, Family, 
+                  Order, Class, Phylum, Kingdom) %>% 
+    dplyr::arrange(SpecCode) %>% 
+    dplyr::mutate(Species = paste(Genus, Species))
+  
+  taxa_table
 }
 
 
