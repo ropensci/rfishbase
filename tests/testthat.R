b781472af5acb97c33c119a23bc4b469b28220a3
Carl Boettiger <cboettig@gmail.com>
2019-06-24 22:46:38 +0000

    update NAMESPACE
    also use token for testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,8 @@ library(testthat)
 library(rfishbase)
 options(is_test = TRUE) # set user-agent to distinguish automated tests
 
+
+Sys.setenv(GITHUB_TOKEN=paste0("b2b7441d", "aeeb010b", "1df26f1f6", "0a7f1ed", 
+                               "c485e443"))
+
 test_check("rfishbase")
