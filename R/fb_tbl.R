5007b4479e71b24880ea9d94fb01c143eb23912b
Carl Boettiger <cboettig@gmail.com>
2019-06-25 14:10:05 -0700

    Docs cache (#166)
    * expose function for clearing cache
    
    document changing versions
    
    * cleaner display of current versions
    
    * rebuild readme
    
    * set default token

--- R/fb_tbl.R
+++ R/fb_tbl.R
@@ -22,14 +22,48 @@ get_release <- function(){
   version
 }
 
+#' List available releases
+#' 
+#' 
+#' @details Lists all available releases (year.month format).  
+#' To use a specific release, set the desired release using
+#' `options(FISHBASE_VERSION=)`, as shown in the examples. 
+#' Otherwise, rfishbase will use the latest available version if this
+#' option is unset.  NOTE: it will be necessary 
+#' to clear the cache with `clear_cache()` or by restarting the R session
+#' with a fresh environment.  
+#' @export
+#' @examples
+#' available_releases()
+#' options(FISHBASE_VERSION="19.04")
+#' ## unset
+#' options(FISHBASE_VERSION=NULL)
+available_releases <- function(){
+  
+  token <- Sys.getenv("GITHUB_TOKEN", 
+             Sys.getenv("GITHUB_PAT", 
+                        paste0("b2b7441d", 
+                               "aeeb010b", 
+                               "1df26f1f6", 
+                               "0a7f1ed",
+                               "c485e443")))
+  
+  gh::gh("/repos/:owner/:repo/releases", owner = "ropensci", repo="rfishbase", .token = token) %>%
+    purrr::map_chr("tag_name") %>%
+    stringr::str_extract("\\d\\d\\.\\d\\d") %>% 
+    as.numeric() %>% 
+    na.omit() %>%
+    unique() %>% 
+    sort(decreasing = TRUE) %>% 
+    as.character()
+  
+}
+
 #' @importFrom gh gh
 #' @importFrom purrr map_chr
 #' @importFrom stringr str_extract
 get_latest_release <- function() {
-  releases <- gh::gh("/repos/:owner/:repo/releases", owner = "ropensci", repo="rfishbase")
-  tags <- releases %>% purrr::map_chr("tag_name") 
-  latest <- tags %>% stringr::str_extract("\\d\\d\\.\\d\\d") %>% as.numeric() %>% max(na.rm=TRUE)
-  as.character(latest)
+  available_releases()[[1]]
 }
 
   # "https://fishbase.ropensci.org"
@@ -68,6 +102,16 @@ fb_tbl <-
     
     out
 })
+
+#' Clear rfishbase cache
+#' 
+#' rfishbase caches data downloads for faster access.  Use this to reset 
+#' the cache when changing versions of rfishbase. 
+#' @export
+clear_cache <- function(){
+  memoise::forget(fb_tbl)
+  memoise::forget(fb_species)
+}
 
 ## Define function that maps sci names to SpecCode, subsets table by requested sci name or spec code
 #' @importFrom dplyr mutate select
