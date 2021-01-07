c2b41a77e230687c840163e7a3cb7d9005049c36
Carl <cboettig@gmail.com>
2021-01-07 20:57:37 +0000

    dummy imports

--- R/cran-hacks.R
+++ R/cran-hacks.R
@@ -1,7 +1,10 @@
+
+#' @importFrom progress progress_bar
+#' @importFrom dbplyr translate_sql
 dummy <- function(){
-  
   # dbplyr is required for the database backend, but is only suggested by dplyr
   # Consequently, functions use dplyr
+  x <- 2
   dbplyr::translate_sql(x + 1)
   progress::progress_bar$new(total = 100)
   NULL
