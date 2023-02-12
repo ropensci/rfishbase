6487ececf2327bb9296a1c12393ef884232ce497
Carl Boettiger <cobettig@gmail.com>
2023-02-12 04:34:49 +0000

    in-mem test, dont need user_data_dir set

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,7 @@
 library(testthat)
 library(rfishbase)
-options(is_test = TRUE) # set user-agent to distinguish automated tests
-
-Sys.setenv(R_USER_DATA_DIR=tempdir())
+#options(is_test = TRUE) # set user-agent to distinguish automated tests
+#Sys.setenv(R_USER_DATA_DIR=tempdir())
 
 
 test_check("rfishbase")
