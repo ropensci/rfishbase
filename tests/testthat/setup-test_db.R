5ff8398cbd2243d89b720179001e66a2e515220a
Carl Boettiger <cobettig@gmail.com>
2023-02-12 04:20:10 +0000

    test using defaults (online-only)

--- tests/testthat/setup-test_db.R
+++ tests/testthat/setup-test_db.R
@@ -1,6 +1,6 @@
 
-test_db <- file.path(tempdir(), "rfishbase")
-dir.create(test_db, showWarnings = FALSE)
-Sys.setenv(FISHBASE_HOME=test_db)
-options(rfishbase_local_prov=TRUE)
-options(rfishbase_local_db=TRUE)
+#test_db <- file.path(tempdir(), "rfishbase")
+#dir.create(test_db, showWarnings = FALSE)
+#Sys.setenv(FISHBASE_HOME=test_db)
+#options(rfishbase_local_prov=TRUE)
+#options(rfishbase_local_db=TRUE)
