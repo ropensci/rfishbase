5af36cf10c4e4398c4c2e5e34e54c72e6408df34
Carl Boettiger <cboettig@berkeley.edu>
2026-04-06 08:00:28 -0700

    Roxygen docs, version bump to 5.0.3, update tests

--- tests/testthat/test_load_taxa.R
+++ tests/testthat/test_load_taxa.R
@@ -1,13 +1,17 @@
 context("load taxa")
 
-test_that("We can load taxa", {
-
+test_that("fishbase loads", {
   needs_api()
   df <- load_taxa(server = "fishbase")
   expect_is(df, "tbl")
-  
-  
-  df <- load_taxa(server="sealifebase")
-  expect_is(df, "tbl")
+})
 
+test_that("sealifebase loads with expected structure", {
+  df <- load_taxa(server = "sealifebase")
+  expect_true(inherits(df, "tbl"))
+  expect_true(all(
+    c("SpecCode", "Species", "Genus", "Family", "Order", "Class") %in%
+      colnames(df)
+  ))
+  expect_gt(nrow(df), 0)
 })
